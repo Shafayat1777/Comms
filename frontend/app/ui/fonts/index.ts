1c07c2bbb53a0d5ccc5cbd41cbb5b9a6dabfe977
shafayat <shafayat1777@gmail.com>
2025-03-05 00:43:03 +0600

    feat: :sparkles: frontend changes

--- frontend/app/ui/fonts/index.ts
+++
@@ -1,5 +0,0 @@
-import { Roboto_Slab, Monoton } from "next/font/google";
-
-export const RobotoSlab = Roboto_Slab({ subsets: ["latin"] });
-
-export const MonoTon = Monoton({ weight: ["400"] });
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+
+export default function Home() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  return (
+    <div className="">
+      {" "}
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border shadow "
+      />
+    </div>
+  );
+}
