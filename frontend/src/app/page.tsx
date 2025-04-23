6105859cfdf4ec5e48a97fc006552e2471346040
Shafayat <shafayat1777@gmail.com>
2025-04-23 21:25:04 +0600

    ci: :sparkles: Added initial side nav

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,26 @@
-
+import { Ellipsis, MessageSquare, Phone, Users } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-    page
+    <div className="grid grid-cols-[25rem_1fr_25rem] flex-1">
+      <div className="border-r">
+        <div className="border-b shadow flex gap-4 p-4">
+          <div className="p-2 rounded hover:bg-primary-foreground cursor-pointer transition">
+            <MessageSquare size={20} />
+          </div>
+          <div className="p-2 rounded hover:bg-primary-foreground cursor-pointer transition">
+            <Phone size={20} />
+          </div>
+          <div className="p-2 rounded hover:bg-primary-foreground cursor-pointer transition">
+            <Users size={20} />
+          </div>
+          <div className="p-2 rounded hover:bg-primary-foreground cursor-pointer transition">
+            <Ellipsis size={20} />
+          </div>
+        </div>
+      </div>
+      <div className="">grid 2</div>
+      <div className="border-l">grid 3</div>
     </div>
   );
 }
